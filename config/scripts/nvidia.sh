9a99d987bb5d857f84dac4888fcb4f67e4d42e71
COMEXR B.V <64539804+comexr@users.noreply.github.com>
2024-05-02 17:43:46 +0200

    Remove nvidia script + reference

--- config/scripts/nvidia.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell this script to exit if there are any errors.
-# You should have this in every custom script, to ensure that your completed
-# builds actually ran successfully without any errors!
-set -oue pipefail
-
-#Blacklist nouveau
-sudo rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
